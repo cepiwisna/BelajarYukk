4b23b8140ff1ab0c84a0ebeadadd4dd969180f59
sanihamdhani <sani060200@gmail.com>
2021-01-03 16:43:34 +0700

    menambahkan file login dan isinya

--- lib/login.dart
+++ lib/login.dart
@@ -6,11 +6,107 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  TextEditingController username = TextEditingController();
+  TextEditingController password = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Text("Kelompok 5"),
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Container(
+          decoration: new BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("images/background.jpg"),
+                  fit: BoxFit.cover)),
+          child: Container(
+            margin: EdgeInsets.all(10),
+            child: Container(
+              child: ListView(
+                // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  SizedBox(height: 250),
+                  Container(
+                    child: Text(
+                      "LOGIN",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  TextField(
+                    controller: username,
+                    onChanged: (value) {
+                      setState(() {});
+                    },
+                    decoration: InputDecoration(
+                      labelText: "Username",
+                      hintText: "Username",
+                      prefixIcon: Icon(Icons.person),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(13),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  TextField(
+                    controller: password,
+                    onChanged: (value) {
+                      setState(() {});
+                    },
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: "Password",
+                      hintText: "Password",
+                      prefixIcon: Icon(Icons.vpn_key_outlined),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(13),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  RaisedButton(
+                      child: Container(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(13),
+                          child: Text("Login"),
+                        ),
+                      ),
+                      padding:
+                          EdgeInsets.symmetric(vertical: 20, horizontal: 100),
+                      onPressed: () {
+                        // if (username.text == "sani" && pass.text == "123") {
+                        //   Navigator.pushReplacement(
+                        //       context,
+                        //       MaterialPageRoute(
+                        //           builder: (context) => DashBoard(
+                        //                 nama: username.text,
+                        //               )));
+                        // } else {
+                        //   AlertDialog alertDialog = new AlertDialog(
+                        //       content: new Text(
+                        //           "Username Atau Password Yang Anda Masukan Salah Atau Belum Di Masukan"));
+                        showDialog(
+                            context: context,
+                            // child: alertDialog,
+                            barrierDismissible: false);
+                      }),
+                  FlatButton(
+                    child: Text(
+                      "Belum Punya Akun?",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
